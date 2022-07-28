ac2bd48e25d425a7042dd3b0e963620238fa65f6
Fernando Bonfim <devnoob@vivaldin.net>
2022-07-28 19:14:41 -0300

    Added cards gallery

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // Import components
 import Header from "./components/header.js"
 import Experiences from './components/experiences.js'
+import CardsGallery from './components/cards-gallery.js'
 
 export default function App() {
   return (
     <div className="container">
       <Header />
       <Experiences />
+      <CardsGallery />
     </div>
   )
 
