0a17b71a431981b4686faf7c3b37f25d2b57e6bb
Fernando Bonfim <devnoob@vivaldin.net>
2022-07-28 19:23:57 -0300

    Added arguments to compoenent.

--- src/components/cards-gallery.js
+++ src/components/cards-gallery.js
@@ -1,8 +1,15 @@
 import Cards from './cards.js'
+
 export default function CardsGallery() {
   return(
       <div className="cards-gallery">
-        <Cards />
+        <Cards
+          img="katie-zafares.jpg"
+          rating="5.0"
+          reviewCount = "(6)"
+          country = "Brazil"
+          price = "123"
+        />
       </div>
   )
 }
